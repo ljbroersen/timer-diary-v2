6689183fb03e1468d29e82cd65f2921f16287ce4
Lisa Broersen <lj.broersen@gmail.com>
2025-05-11 01:01:35 +0900

    feat: installed and set up vite and tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "800px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
+};
