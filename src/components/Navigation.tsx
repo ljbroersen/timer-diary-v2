02b6a48dfaa924ae6e8f0efa427d8be9f58bca55
Lisa Broersen <lj.broersen@gmail.com>
2025-06-07 17:53:44 +0900

    fix: changed the whole layout of the navigation/calendar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import Calendar from "react-calendar";
 import type { NavigationProps } from "../types/types";
-import "../calendar.css"
+import "../calendar.css";
 
 function formatDate(date: Date): string {
   return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(
@@ -15,8 +15,8 @@ export default function Navigation({
   setVisibleLogIndex,
 }: NavigationProps) {
   return (
-    <div>
-      <h2>Calendar</h2>
+    <div className="bg-[rgb(var(--color-bg-primary))] rounded-xl border shadow-md p-4 max-h-[calc(100%-2rem)]">
+      <h2 className="text-lg font-semibold mb-4">Calendar</h2>
       <Calendar
         onChange={(date) => {
           setSelectedDate(date as Date);
@@ -27,6 +27,7 @@ export default function Navigation({
           const formattedDate = formatDate(date);
           return dates.some((d) => d.date === formattedDate) ? "highlight" : "";
         }}
+        className="w-full"
       />
     </div>
   );
